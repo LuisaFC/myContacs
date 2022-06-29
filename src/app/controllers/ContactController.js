41ff96c69b38a7f35ffffddddc7ae86e48fa1261
LuisaFC <luisabsb@gmail.com>
2022-06-29 18:34:04 -0300

    feat: update route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -32,7 +32,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return response.status(400).json({ error: 'This e-mail is already been taken' });
+      return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -42,8 +42,33 @@ class ContactController {
     return response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Alterar registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(400).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    return response.json(contact);
   }
 
   async delete(request, response) {
